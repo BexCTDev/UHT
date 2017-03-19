4918eb31092c4bfd91e6ce226f01fed2223f80d9
BexCTDev <developer@bexct.com>
2017-03-19 16:48:45 -0400

    Baseic Child Themme

---
+++ themes/UnlimitedHomeTheate/functions.php
@@ -0,0 +1,25 @@
+<?php
+/* 
+ * Unlimited Home Theatre functions.
+ * 
+ * @package UnlimitedHomeTheate
+ * Support : Bex Consutlting Technologies
+ * Website : www.bexct.com
+ * 
+ */
+
+// Queue parent style followed by child/customized style
+
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'twentyseventeen';
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+?>
